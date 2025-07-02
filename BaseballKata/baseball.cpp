ac7ec458f667f2b48186ea6354212831ee941f2e
MJ <julysky40@gmail.com>
2025-07-02 13:11:30 +0900

    [feature] page 30

--- BaseballKata/baseball.cpp
+++ BaseballKata/baseball.cpp
@@ -2,12 +2,19 @@
 #include <string>
 using std::string;
 using std::length_error;
+using std::invalid_argument;
 
 class Baseball {
 public:
 	void guess(const string& string) {
 		if (string.length() != 3) {
 			throw length_error("Must be three letters.");
+		}
+
+		for (char ch : string) {
+			if (ch < '0' || ch >'9') {
+				throw invalid_argument("Must be number.");
+			}
 		}
 	}
 };
--- BaseballKata/baseball_test.cpp
+++ BaseballKata/baseball_test.cpp
@@ -6,6 +6,12 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
