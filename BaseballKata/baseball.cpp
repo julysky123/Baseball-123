d7f3bf96a97acb5ecedf9ac0a2e16849da3b5519
MJ <julysky40@gmail.com>
2025-07-02 13:08:40 +0900

    [feature] page 27

--- BaseballKata/baseball.cpp
+++ BaseballKata/baseball.cpp
@@ -1,4 +1,11 @@
+#include <stdexcept>
+#include <string>
+using std::string;
+using std::length_error;
+
 class Baseball {
 public:
-
+	void guess(const string& string) {
+		throw length_error("Must be three letters.");
+	}
 };
--- BaseballKata/baseball_test.cpp
+++ BaseballKata/baseball_test.cpp
@@ -1,10 +1,11 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+using std::string;
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
-
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
