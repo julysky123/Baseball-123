b7890728a32fb11d9074b3a326dbdef08e416bb4
MJ <julysky40@gmail.com>
2025-07-02 13:16:07 +0900

    [refactoring] page 37

--- BaseballKata/baseball_test.cpp
+++ BaseballKata/baseball_test.cpp
@@ -1,15 +1,27 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+using namespace::testing;
 using std::string;
+using std::exception;
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+class BaseballFixture : public Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
-}
+	void assertIllegalArgument(string guessNumber) {
+		Baseball game;
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			//PASS
+		}
+	}
+};
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
 
 int main() {
